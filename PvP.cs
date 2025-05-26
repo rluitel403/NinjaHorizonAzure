6c6bf37d50204db00a4d1af77816d5a1dab9e47e
rluitel403 <luitelrubin@gmail.com>
2025-05-26 00:52:30 -0700

    save

--- PvP.cs
+++ PvP.cs
@@ -62,25 +62,10 @@ namespace NinjaHorizon.Function
         public List<PvPCharacterInfo> selectedCharacters { get; set; }
 
         public bool isReady { get; set; }
-        
+
         // Turn counter for each character (stackId -> counter value)
         public Dictionary<string, float> turnCounters { get; set; } = new Dictionary<string, float>();
     }
-
-    public class PvPBattleResult
-    {
-        public bool hasWinner { get; set; }
-        public bool isDraw { get; set; }
-        public string winnerEntityId { get; set; }
-
-    }
-
-    public class GetSharedGroupDataResultWithBattleResult
-    {
-        public Dictionary<string, SharedGroupDataRecord> data { get; set; }
-        public PvPBattleResult battleResult { get; set; }
-    }
-
 
     public static class PvP
     {
@@ -96,10 +81,10 @@ namespace NinjaHorizon.Function
             {
                 SharedGroupId = matchId
             };
-            
+
             PlayFabResult<CreateSharedGroupResult> createSharedGroupResult =
                 await serverApi.CreateSharedGroupAsync(createSharedGroupRequest);
-            
+
             var getMatchResult = await multiplayerApi.GetMatchAsync(
                 new PlayFab.MultiplayerModels.GetMatchRequest
                 {
@@ -107,7 +92,7 @@ namespace NinjaHorizon.Function
                     MatchId = matchId
                 }
             );
-            
+
             int randomNumber = new Random().Next(0, 2);
             string myEntityId = getMatchResult.Result.Members[randomNumber].Entity.Id;
             string enemyEntityId = getMatchResult.Result.Members[(randomNumber + 1) % 2].Entity.Id;
@@ -126,7 +111,7 @@ namespace NinjaHorizon.Function
                     }
                 }
             };
-            
+
             await serverApi.UpdateSharedGroupDataAsync(updateSharedGroupDataRequest);
             return new { };
         }
@@ -142,12 +127,12 @@ namespace NinjaHorizon.Function
                 JsonConvert.DeserializeObject<PvPSelectCharacterInput>(
                     context.FunctionArgument.pvpSelectCharacterInput.ToString()
                 );
-            
+
             var getSharedGroupDataRequest = new GetSharedGroupDataRequest
             {
                 SharedGroupId = matchId,
             };
-            
+
             PlayFabResult<GetSharedGroupDataResult> getSharedGroupDataResult =
                 await serverApi.GetSharedGroupDataAsync(getSharedGroupDataRequest);
 
@@ -160,20 +145,20 @@ namespace NinjaHorizon.Function
                 JsonConvert.DeserializeObject<PlayerSharedGroupData>(
                     getSharedGroupDataResult.Result.Data[enemyEntityId].Value
                 );
-            enemySharedGroupInput.myTurn = true;     
-            mySharedGroupInput.myTurn = false;       
+            enemySharedGroupInput.myTurn = true;
+            mySharedGroupInput.myTurn = false;
             if (mySharedGroupInput.selectedCharacters == null)
             {
                 mySharedGroupInput.selectedCharacters = new List<PvPCharacterInfo>();
             }
-            
+
             // Get selected character data
             await PopulateCharacterDataAsync(economyApi, context, pvpInput.selectedCharacter);
-            
+
             mySharedGroupInput.selectedCharacters.Add(pvpInput.selectedCharacter);
 
             // Check if both sides have selected their characters
-            if (mySharedGroupInput.selectedCharacters.Count == 3 && 
+            if (mySharedGroupInput.selectedCharacters.Count == 3 &&
                 enemySharedGroupInput.selectedCharacters.Count == 3)
             {
                 AssignRandomPlayerTurn(mySharedGroupInput, enemySharedGroupInput);
@@ -211,11 +196,11 @@ namespace NinjaHorizon.Function
                     CollectionId = "default",
                     Filter = "stackId eq '" + character.stackId + "'"
                 };
-            
+
             var selectCharacterInventory = await economyApi.GetInventoryItemsAsync(
                 getSelectCharacterInventoryRequest
             );
-            
+
             EntityData entityData = JsonConvert.DeserializeObject<EntityData>(
                 selectCharacterInventory.Result.Items[0].DisplayProperties.ToString(),
                 new JsonSerializerSettings
@@ -229,19 +214,19 @@ namespace NinjaHorizon.Function
             // Get equipped items data
             string itemFilter = BuildItemFilter(entityData);
             Dictionary<string, PvPItemIdAndTier> itemsTier = new Dictionary<string, PvPItemIdAndTier>();
-            
+
             if (!string.IsNullOrEmpty(itemFilter))
             {
                 itemsTier = await GetEquippedItemsDataAsync(economyApi, context, itemFilter);
             }
-            
+
             character.itemIdAndTier = itemsTier;
         }
 
         private static string BuildItemFilter(EntityData entityData)
         {
             var itemIds = new List<string>();
-            
+
             if (entityData.weapon != null)
                 itemIds.Add(entityData.weapon);
             if (entityData.backItem != null)
@@ -250,8 +235,8 @@ namespace NinjaHorizon.Function
                 itemIds.Add(entityData.clothing);
             if (entityData.artifact != null)
                 itemIds.Add(entityData.artifact);
-            
-            return itemIds.Count > 0 
+
+            return itemIds.Count > 0
                 ? string.Join(" or ", itemIds.Select(id => $"stackId eq '{id}'"))
                 : "";
         }
@@ -271,17 +256,17 @@ namespace NinjaHorizon.Function
                 CollectionId = "default",
                 Filter = itemFilter
             };
-            
+
             PlayFabResult<GetInventoryItemsResponse> equippedItems =
                 await economyApi.GetInventoryItemsAsync(getEquippedItemsRequest);
-            
+
             Dictionary<string, PvPItemIdAndTier> itemIdAndTier = new Dictionary<string, PvPItemIdAndTier>();
             foreach (InventoryItem item in equippedItems.Result.Items)
             {
                 Tier itemData = JsonConvert.DeserializeObject<Tier>(item.DisplayProperties.ToString());
                 itemIdAndTier.Add(item.StackId, new PvPItemIdAndTier() { itemId = item.Id, tier = itemData });
             }
-            
+
             return itemIdAndTier;
         }
 
@@ -290,13 +275,13 @@ namespace NinjaHorizon.Function
             PlayerSharedGroupData enemySharedGroupInput)
         {
             int randomNumber = new Random().Next(0, 2);
-            
+
             if (randomNumber == 0)
             {
                 mySharedGroupInput.myTurn = true;
                 enemySharedGroupInput.myTurn = false;
                 int randomCharacter = new Random().Next(0, mySharedGroupInput.selectedCharacters.Count);
-                mySharedGroupInput.entityCharacterTurnStackId = 
+                mySharedGroupInput.entityCharacterTurnStackId =
                     mySharedGroupInput.selectedCharacters[randomCharacter].stackId;
             }
             else
@@ -304,10 +289,10 @@ namespace NinjaHorizon.Function
                 enemySharedGroupInput.myTurn = true;
                 mySharedGroupInput.myTurn = false;
                 int randomCharacter = new Random().Next(0, enemySharedGroupInput.selectedCharacters.Count);
-                enemySharedGroupInput.entityCharacterTurnStackId = 
+                enemySharedGroupInput.entityCharacterTurnStackId =
                     enemySharedGroupInput.selectedCharacters[randomCharacter].stackId;
             }
-            
+
             mySharedGroupInput.isReady = true;
             enemySharedGroupInput.isReady = true;
         }
@@ -321,14 +306,14 @@ namespace NinjaHorizon.Function
             {
                 SharedGroupId = matchId,
             };
-            
+
             PlayFabResult<GetSharedGroupDataResult> getSharedGroupDataResult =
                 await serverApi.GetSharedGroupDataAsync(getSharedGroupDataRequest);
 
             return JsonConvert.SerializeObject(getSharedGroupDataResult.Result.Data);
         }
 
-        private static PvPBattleResult CheckWinCondition(Dictionary<string, SharedGroupDataRecord> sharedData, string entityId)
+        private static string CheckWinCondition(Dictionary<string, SharedGroupDataRecord> sharedData, string entityId)
         {
             try
             {
@@ -344,41 +329,26 @@ namespace NinjaHorizon.Function
                 if (player1AliveCount == 0 && player2AliveCount == 0)
                 {
                     // Draw - both players have no alive characters
-                    return new PvPBattleResult() 
-                    { 
-                        hasWinner = true, 
-                        isDraw = true, 
-                        winnerEntityId = null
-                    };
+                    return null;
                 }
                 else if (player1AliveCount == 0)
                 {
                     // Player 2 wins
-                    return new PvPBattleResult() 
-                    { 
-                        hasWinner = true, 
-                        isDraw = false, 
-                        winnerEntityId = player2Data.myEntityId 
-                    };
+                    return player2Data.myEntityId;
                 }
                 else if (player2AliveCount == 0)
                 {
                     // Player 1 wins
-                    return new PvPBattleResult() 
-                    { 
-                        hasWinner = true, 
-                        isDraw = false, 
-                        winnerEntityId = player1Data.myEntityId 
-                    };
+                    return player1Data.myEntityId;
                 }
 
                 // No winner yet
-                return new PvPBattleResult() { hasWinner = false };
+                return null;
             }
             catch
             {
                 // If there's any error parsing data, assume no winner
-                return new PvPBattleResult() { hasWinner = false };
+                return null;
             }
         }
 
@@ -396,10 +366,10 @@ namespace NinjaHorizon.Function
             {
                 SharedGroupId = matchId,
             };
-            
+
             PlayFabResult<GetSharedGroupDataResult> getSharedGroupDataResult =
                 await serverApi.GetSharedGroupDataAsync(getSharedGroupDataRequest);
-            
+
             PlayerSharedGroupData mySharedGroup =
                 JsonConvert.DeserializeObject<PlayerSharedGroupData>(
                     getSharedGroupDataResult.Result.Data[entityId].Value
@@ -415,7 +385,7 @@ namespace NinjaHorizon.Function
             {
                 ProcessTurnSwitch(mySharedGroup, enemySharedGroup, turnData);
             }
-            
+
             await serverApi.UpdateSharedGroupDataAsync(
                 new UpdateSharedGroupDataRequest
                 {
@@ -428,7 +398,7 @@ namespace NinjaHorizon.Function
                     }
                 }
             );
-            
+
             return new { };
         }
 
@@ -440,22 +410,22 @@ namespace NinjaHorizon.Function
             // Update character data from the battle results
             mySharedGroup.selectedCharacters = turnData.playerData.selectedCharacters;
             enemySharedGroup.selectedCharacters = turnData.enemyData.selectedCharacters;
-            
+
             const float TURN_THRESHOLD = 100f;
-            
+
             // Initialize turn counters if needed
             InitializeTurnCounters(mySharedGroup, enemySharedGroup);
-            
+
             // Get all alive characters from both players
             var allCharacters = new List<PvPCharacterInfo>();
             allCharacters.AddRange(mySharedGroup.selectedCharacters);
             allCharacters.AddRange(enemySharedGroup.selectedCharacters);
-            
+
             if (allCharacters.Count == 0) return;
-            
+
             // Calculate how many cycles each entity needs to reach the threshold
             float minCyclesNeeded = float.MaxValue;
-            
+
             foreach (var character in allCharacters)
             {
                 float speedIncrement = character.baseSpeed / 100f;
@@ -466,55 +436,55 @@ namespace NinjaHorizon.Function
                     minCyclesNeeded = Math.Min(minCyclesNeeded, cyclesNeeded);
                 }
             }
-            
+
             // Apply the minimum cycles needed to all entities and find ready entities
             var readyCharacters = new List<(PvPCharacterInfo character, float turnCounter)>();
-            
+
             foreach (var character in allCharacters)
             {
                 float speedIncrement = character.baseSpeed / 100f;
                 float currentCounter = GetTurnCounter(mySharedGroup, enemySharedGroup, character.stackId);
                 float newCounter = currentCounter + (speedIncrement * minCyclesNeeded);
-                
+
                 // Update the turn counter
                 UpdateTurnCounter(mySharedGroup, enemySharedGroup, character.stackId, newCounter);
-                
+
                 if (newCounter >= TURN_THRESHOLD)
                 {
                     readyCharacters.Add((character, newCounter));
                 }
             }
-            
+
             // Sort ready entities based on their turn counter values and entity ID for tiebreaker
             readyCharacters.Sort((a, b) =>
             {
                 int turnComparison = b.turnCounter.CompareTo(a.turnCounter);
                 if (turnComparison != 0) return turnComparison;
-                
+
                 // Use entity ID as tiebreaker - determine which player owns each character
-                string aOwnerEntityId = mySharedGroup.selectedCharacters.Any(c => c.stackId == a.character.stackId) 
-                    ? mySharedGroup.myEntityId 
+                string aOwnerEntityId = mySharedGroup.selectedCharacters.Any(c => c.stackId == a.character.stackId)
+                    ? mySharedGroup.myEntityId
                     : enemySharedGroup.myEntityId;
-                string bOwnerEntityId = mySharedGroup.selectedCharacters.Any(c => c.stackId == b.character.stackId) 
-                    ? mySharedGroup.myEntityId 
+                string bOwnerEntityId = mySharedGroup.selectedCharacters.Any(c => c.stackId == b.character.stackId)
+                    ? mySharedGroup.myEntityId
                     : enemySharedGroup.myEntityId;
-                
+
                 return string.Compare(bOwnerEntityId, aOwnerEntityId, StringComparison.Ordinal);
             });
-            
+
             // Get the character with the next turn
             if (readyCharacters.Count > 0)
             {
                 var nextTurnCharacter = readyCharacters[0].character;
-                
+
                 // Subtract threshold from the selected character's turn counter
                 float currentCounter = GetTurnCounter(mySharedGroup, enemySharedGroup, nextTurnCharacter.stackId);
                 UpdateTurnCounter(mySharedGroup, enemySharedGroup, nextTurnCharacter.stackId, currentCounter - TURN_THRESHOLD);
-                
+
                 // Determine which player owns this character and assign turn
                 bool isMyCharacterTurn = mySharedGroup.selectedCharacters
                     .Any(c => c.stackId == nextTurnCharacter.stackId);
-                
+
                 if (isMyCharacterTurn)
                 {
                     mySharedGroup.myTurn = true;
@@ -536,21 +506,21 @@ namespace NinjaHorizon.Function
                 mySharedGroup.turnCounters = new Dictionary<string, float>();
             if (enemySharedGroup.turnCounters == null)
                 enemySharedGroup.turnCounters = new Dictionary<string, float>();
-                
+
             // Initialize counters for new characters
             foreach (var character in mySharedGroup.selectedCharacters)
             {
                 if (!mySharedGroup.turnCounters.ContainsKey(character.stackId))
                     mySharedGroup.turnCounters[character.stackId] = 0f;
             }
-            
+
             foreach (var character in enemySharedGroup.selectedCharacters)
             {
                 if (!enemySharedGroup.turnCounters.ContainsKey(character.stackId))
                     enemySharedGroup.turnCounters[character.stackId] = 0f;
             }
         }
-        
+
         private static float GetTurnCounter(PlayerSharedGroupData mySharedGroup, PlayerSharedGroupData enemySharedGroup, string stackId)
         {
             if (mySharedGroup.turnCounters?.ContainsKey(stackId) == true)
@@ -559,7 +529,7 @@ namespace NinjaHorizon.Function
                 return enemySharedGroup.turnCounters[stackId];
             return 0f;
         }
-        
+
         private static void UpdateTurnCounter(PlayerSharedGroupData mySharedGroup, PlayerSharedGroupData enemySharedGroup, string stackId, float value)
         {
             if (mySharedGroup.turnCounters?.ContainsKey(stackId) == true)
@@ -577,15 +547,11 @@ namespace NinjaHorizon.Function
             {
                 SharedGroupId = matchId,
             };
-            
+
             PlayFabResult<GetSharedGroupDataResult> getSharedGroupDataResult =
                 await serverApi.GetSharedGroupDataAsync(getSharedGroupDataRequest);
-            
-            return JsonConvert.SerializeObject(new GetSharedGroupDataResultWithBattleResult()
-            {
-                data = getSharedGroupDataResult.Result.Data,
-                battleResult = CheckWinCondition(getSharedGroupDataResult.Result.Data, entityId)
-            });
+
+            return JsonConvert.SerializeObject(getSharedGroupDataResult.Result.Data);
         }
 
         [FunctionName("PvP")]
@@ -606,11 +572,11 @@ namespace NinjaHorizon.Function
             {
                 EntityToken = context.TitleAuthenticationContext.EntityToken
             };
-            
+
             var serverApi = new PlayFabServerInstanceAPI(apiSettings, titleContext);
             var economyApi = new PlayFabEconomyInstanceAPI(apiSettings, titleContext);
             var multiplayerApi = new PlayFabMultiplayerInstanceAPI(apiSettings, titleContext);
-            
+
             string entityId = context.CallerEntityProfile.Entity.Id;
             string actionType = context.FunctionArgument.actionType;
             string matchId = context.FunctionArgument.matchId;
@@ -619,20 +585,20 @@ namespace NinjaHorizon.Function
             {
                 case "CreateSharedGroup":
                     return await CreateSharedGroupAsync(serverApi, multiplayerApi, matchId, entityId);
-                
+
                 case "SelectCharacter":
                     return await SelectCharacterAsync(serverApi, economyApi, context, matchId, entityId);
-                
+
                 case "GetSharedGroupData":
                     return await GetSharedGroupDataAsync(serverApi, matchId, entityId);
-                
+
                 case "UpdateTurnData":
                     return await UpdateTurnDataAsync(serverApi, context, matchId, entityId);
-                
+
                 case "GetTurnData":
                     return await GetTurnDataAsync(serverApi, matchId, entityId);
             }
-            
+
             return new { };
         }
     }
